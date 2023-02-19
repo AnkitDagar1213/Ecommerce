085746917fba032d439ed0b6d0a273808679adcf
Dagarshab <112771048+Dagarshab@users.noreply.github.com>
2023-02-19 22:54:12 +0530

    Update Address.java

--- Ecommerce/src/main/java/com/example/Ecommerce/model/Address.java
+++ Ecommerce/src/main/java/com/example/Ecommerce/model/Address.java
@@ -1,6 +1,7 @@
 package com.example.Ecommerce.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,13 +16,24 @@ import lombok.NoArgsConstructor;
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Address_Id")
     private Integer AddressId;
+    @NotBlank(message = "Can't be null")
+    @Column(name = "name")
     private String Name;
+    @NotBlank(message = "Can't be null")
+    @Column(name = "landmark")
     private String Landmark;
+    @NotBlank(message = "Can't be null")
+    @Column(name = "phone_number")
     private String Phone_number;
+    @NotBlank(message = "Can't be null")
+    @Column(name = "zipcode")
     private String Zipcode;
+    @NotBlank(message = "Can't be null")
+    @Column(name = "state")
     private String state;
-    @JoinColumn
-    @OneToMany
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User UserID;
 }
