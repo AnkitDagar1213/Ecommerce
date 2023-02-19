a902beab3cd6a2c70d0f3d5e5ca328edb6457fbc
Dagarshab <112771048+Dagarshab@users.noreply.github.com>
2023-02-19 22:55:25 +0530

    Update Productcontroller.java

--- Ecommerce/src/main/java/com/example/Ecommerce/controller/Productcontroller.java
+++ Ecommerce/src/main/java/com/example/Ecommerce/controller/Productcontroller.java
@@ -1,20 +1,30 @@
 package com.example.Ecommerce.controller;
 
 import com.example.Ecommerce.model.Product;
-import com.example.Ecommerce.repository.ProductRepository;
 import com.example.Ecommerce.service.Productservice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/Product")
+@RequestMapping("/api/Product")
 public class Productcontroller {
     @Autowired
-    private ProductRepository productRepository;
-    @Autowired
     private Productservice productservice;
+    @GetMapping("/search")
+    public ResponseEntity<Optional<Product>> getProductsByCategory(@RequestParam(value = "category") Integer Id) {
+        Optional<Product> products = productservice.getProductsByCategory(Id);
+        return ResponseEntity.ok().body(products);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+        Product newProduct = productservice.addProduct(product);
+        return ResponseEntity.ok().body(newProduct);
+    }
     @GetMapping("/getAllProduct")
     public List<Product> findAllProduct() {
         return productservice.findAll();
