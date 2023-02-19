00b607c5ec8d696f6aea5ae7a997af64ec2015c9
Dagarshab <112771048+Dagarshab@users.noreply.github.com>
2023-02-19 22:53:22 +0530

    Update Product.java

--- Ecommerce/src/main/java/com/example/Ecommerce/model/Product.java
+++ Ecommerce/src/main/java/com/example/Ecommerce/model/Product.java
@@ -1,6 +1,8 @@
 package com.example.Ecommerce.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,10 +17,21 @@ import lombok.NoArgsConstructor;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Product_Id")
     private Integer ProductId;
+    @NotBlank(message = "Can't be null")
+    @Column(name = "name")
     private String Name;
+    @NotNull(message = "Can't be null")
+    @Column(name = "price")
     private String Price;
+    @NotBlank(message = "Can't be null")
+    @Column(name = "description")
     private String Description;
+    @NotBlank(message = "Can't be null")
+    @Column(name = "category")
     private String Category;
+    @NotBlank(message = "Can't be null")
+    @Column(name = "brand")
     private String Brand;
 }
