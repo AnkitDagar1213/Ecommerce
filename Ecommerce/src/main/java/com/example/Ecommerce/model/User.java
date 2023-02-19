b55ed0f0bb1c8bbce4dfa0c200c1ce51f28388ed
Dagarshab <112771048+Dagarshab@users.noreply.github.com>
2023-02-19 22:52:36 +0530

    Update User.java

--- Ecommerce/src/main/java/com/example/Ecommerce/model/User.java
+++ Ecommerce/src/main/java/com/example/Ecommerce/model/User.java
@@ -1,6 +1,7 @@
 package com.example.Ecommerce.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +16,18 @@ import lombok.NoArgsConstructor;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "UserId")
     private Integer UserId;
+    @NotNull(message = "Can't be null")
+    @Column(name = "Name")
     private String Name;
+    @NotNull(message = "Can't be null")
+    @Email(message = "Invalid email address")
+    @Column(name = "Email")
     private String Email;
+    @NotNull(message = "Can't be null")
+    @Column(name = "Password")
     private String Password;
+    @Column(name = "Phone_number")
     private String Phone_number;
 }
